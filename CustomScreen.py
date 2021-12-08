4cbb3d469ff1be2b6ccbe35366d902c6da39bcc5
Messminder <Shwadow@protonmail.ch>
2021-12-08 07:04:13 -0500

    tired but glad i made something...

--- CustomScreen.py
+++ CustomScreen.py
@@ -3,28 +3,24 @@ from rich.align import Align
 from rich.text import Text
 from rich.style import Style
 from textual.app import App
-from textual.events import Click
 from textual.reactive import Reactive
 from textual.widget import Widget
-from textual.widgets import Placeholder
 import os
 import psutil
 from psutil._common import bytes2human
 import time
 import platform
 import distro
-import GPUtil
 import subprocess
 import cpuinfo
 from datetime import datetime
 
 # OS Screen contains software centric system information and returns it wrapped in a panel.
-# TODO -Get host name and uptime since boot in a politely readable manner
 class OSScreen(Widget):
     def render(self):
         # Execute shell command using package "wmctrl" to identify window manager and desktop environment. How this works is currently unknown.
         desktopsoftwareinfo = subprocess.check_output("wmctrl -m", shell=True, text=True)
-        # Sub string extraction from the returned value of the shell command above, identifying my own window manager.
+       # Sub string extraction from the returned value of the shell command above, identifying my own window manager.
         wmsubstring = (desktopsoftwareinfo[6:9])
         # Window title
         OStitleText = "[bold cyan]OPERATING SYSTEM[/bold cyan]"
@@ -45,8 +41,6 @@ class OSScreen(Widget):
 
 
 # HW Screen contains Hardware information and specifications.
-# All TODO completed
-
 class HWScreen(Widget):
    # cpuinfo is quite of a slow module. Perhaps it is the nature of interpreted languages, the fact that textual is a module in it's infancy, or (most likely) just me being terrible at programming. Or maybe because i'm coding on an old HP Compaq with a grand max of 2.50GHz processing which doesn't help python or whatsoever.
    # What i do know is that the hypothesis of "Does textual play nice with two widgets (or more) that refreshes at the same time?" is "NO" (or at least seems to be) and it was tested with a widget made with rich (Clock(Widget))
@@ -58,7 +52,9 @@ class HWScreen(Widget):
 
    # render stuff like text here.
     def render(self):
+        # Hardware screen title text
         HWtitleText = "[bold cyan]HARDWARE INFORMATION[/bold cyan]"
+        # The text inside - disk_usage takes for argument a disk partition.
         hwinfo = f"""[bold green]CPU:[/bold green][green] {cpuinfo.get_cpu_info()["brand_raw"]} [/green]
    [bold green]ARCHITECTURE:[/bold green][green] {cpuinfo.get_cpu_info()["arch_string_raw"] + " " + "(" +  str(cpuinfo.get_cpu_info()["bits"]) + "-bits" + ")"} [/green]
    [bold green]VENDOR:[/bold green][green] {cpuinfo.get_cpu_info()["vendor_id_raw"]} [/green]
@@ -68,7 +64,7 @@ class HWScreen(Widget):
           [green]TOTAL: {bytes2human(psutil.virtual_memory().total)}[/green]
           [green]USED: {bytes2human(psutil.virtual_memory().used) + " (" + str(psutil.virtual_memory().percent) + "%)"}[/green]
           [green]AVAIALBLE: {bytes2human(psutil.virtual_memory().available)}[/green]
-       [bold green]HDD:[/bold green]
+      [bold green]HDD:[/bold green]
           [green]TOTAL: {bytes2human(psutil.disk_usage("/").total)}[/green]
           [green]USED: {bytes2human(psutil.disk_usage("/").used) + " (" + str(psutil.disk_usage("/").percent) + "%)"}[/green]
           [green]AVAILABLE: {bytes2human(psutil.disk_usage("/").free)}[/green]
@@ -87,12 +83,13 @@ class Clock(Widget):
         currentTime = Panel(Text(datetime.now().strftime("%c")), title="EXEC TIME")
         return (Align.center(currentTime, vertical="top"))
 
-# App initializations
+# App initialization
 class Test(App):
     async def on_mount(app) -> None:
+        clocksize = 28
         # Dock both main screen widgets to the left at Z layer -1. Size is current terminal size minus clock widget size divided by 2.
-        await app.view.dock(OSScreen(), HWScreen(),  z=-1, edge="left", size=int((os.get_terminal_size().columns - 28) /2))
-        await app.view.dock(Clock(), edge="right", z=0, size=28)
+        await app.view.dock(OSScreen(), HWScreen(),  z=-1, edge="left", size=int((os.get_terminal_size().columns - clocksize) /2))
+        await app.view.dock(Clock(), edge="right", z=0, size=clocksize)
 
 
 if __name__ == '__main__':
